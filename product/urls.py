63a7bfa20c2724c204e5d8df784eee9df6942228
masurovaa <masurova09@gmail.com>
2025-05-11 19:36:47 +0300

    HW3

--- product/urls.py
+++ product/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('categories/', CategoryListAPIViews.as_view()),
+    path('categories/', CategoryListCreateView.as_view()),
     path('categories/<int:id>/', CategoryDetailView.as_view()),
-    path('products/', ProductListView.as_view()),
+    path('products/', ProductListCreateView.as_view()),
     path('products/<int:id>/', ProductDetailView.as_view()),
     path('products/reviews/', ProductReviewsListAPIView.as_view()),
-    path('reviews/', ReviewListView.as_view()),
+    path('reviews/', ReviewListCreateView.as_view()),
     path('reviews/<int:id>/', ReviewDetailView.as_view()),
+    path('categories/stats/', CategoryStatsAPIView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,35 +1,35 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Category, Product, Review
 from .serializers import (CategorySerializer, ProductSerializer,
                           ReviewSerializer, ProductWithReviewsSerializer)
 from django.db.models import Avg, Count
 
 
-class CategoryListView(ListAPIView):
+class CategoryListCreateView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class CategoryDetailView(RetrieveAPIView):
+class CategoryDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'id'
 
-class ProductListView(ListAPIView):
+class ProductListCreateView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class ProductDetailView(RetrieveAPIView):
+class ProductDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'id'
 
-class ReviewListView(ListAPIView):
+class ReviewListCreateView(ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-class ReviewDetailView(RetrieveAPIView):
+class ReviewDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     lookup_field = 'id'
@@ -40,7 +40,7 @@ class ProductReviewsListAPIView(APIView):
         serializer = ProductWithReviewsSerializer(products, many=True)
         return Response(serializer.data)
     
-class CategoryListAPIViews(APIView):
+class CategoryStatsAPIView(APIView):
     def get(self, request):
         categories = Category.objects.annotate(products_count=Count('products'))
         serializer = CategorySerializer(categories, many=True)
